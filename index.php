dac6d7f4a3ed46e9218bd990d46ae91195371c39
jordandmg <dominguez.jordan1@gmail.com>
2019-01-28 15:28:31 +0100

    Mise a jour style

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 ?>
 <!DOCTYPE html>
@@ -9,13 +9,18 @@ session_start();
         <title>Pendu</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <link rel="steelsheet" href="style.css">
     </head>
     <body>
-        
-        
-        <form method="post" action="pendu.php">
-            <input type="text" name="motAtrouver" />
-            <input type="submit" id="send" value="Envoyer" />
-        </form>
+
+        <div class="container">
+            <!-- Page Content goes here -->
+            <h2 class="center blue-text text-darken-2">Le Pendu</h2>
+            <span class="flow-text">Veuillez entrer un mot : </span>
+            <form method="post" action="pendu.php">
+                <input type="text" name="motAtrouver" pattern="[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ\-\']+" required />
+                <input class=" btn waves-effect waves-light card-panel blue darken-2" type="submit" id="send" value="Envoyer" />
+            </form>
+        </div>
     </body>
 </html>
--- pendu.php
+++ pendu.php
@@ -14,32 +14,44 @@ function skip_accents($str, $charset = 'utf-8') {
 
 if (isset($_POST['motAtrouver'])) {
     $unionTcheck = strpos($_POST['motAtrouver'], "-");
-    $_SESSION['mot'] = skip_accents($_POST['motAtrouver']);
+    $apostropheTcheck = strpos($_POST['motAtrouver'], "'");
+    $mot = skip_accents($_POST['motAtrouver']);
+
+    $_SESSION['mot'] = $mot;
 }
 
 
-$nblettre = strlen($_POST['motAtrouver']);
+$nblettre = strlen($mot);
 $letterArray = array();
-$i=0;
+$i = 0;
 while ($i != $nblettre) {
 
-    
-    $letterArray[$i]= "_ ";
-    if (isset($unionTcheck)) {
+
+    $letterArray[$i] = "_ ";
+    if ($unionTcheck != 0) {
         if ($unionTcheck == $i) {
+
             $letterArray[$i] = "-";
         }
     }
+
+    if ($apostropheTcheck != 0) {
+        if ($apostropheTcheck == $i) {
+
+            $letterArray[$i] = "'";
+        }
+    }
+
     $i++;
 }
-var_dump($letterArray);
+
 $hiddenWord = "";
 $i = 0;
+
 foreach ($letterArray as $letter) {
-    $hiddenWord .= "<p id=" . $i . ">$letter</p>";
+    $hiddenWord .= "<span style='font-size:3em; text-align=center;' id=" . $i . ">$letter</span>";
     $i++;
 }
-echo $hiddenWord;
 ?>
 <!DOCTYPE html>
 
@@ -48,48 +60,62 @@ echo $hiddenWord;
         <meta charset="UTF-8">
         <title></title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+        <style>
+
+        </style>
 
     </head>
     <body>
 
-        <div id="requestajax"></div>
-        </BR><button type="button" id="A">A</button>
-        <button type="button" id="Z">Z</button>
-        <button type="button" id="E">E</button>
-        <button type="button" id="R">R</button>
-        <button type="button" id="T">T</button>
-        <button type="button" id="Y">Y</button>
-        <button type="button" id="U">U</button>
-        <button type="button" id="I">I</button>
-        <button type="button" id="O">O</button>
-        <button type="button" id="P">P</button></BR>
-        <button type="button" id="Q">Q</button>
-        <button type="button" id="S">S</button>
-        <button type="button" id="D">D</button>
-        <button type="button" id="F">F</button>
-        <button type="button" id="G">G</button>
-        <button type="button" id="H">H</button>
-        <button type="button" id="J">J</button>
-        <button type="button" id="K">K</button>
-        <button type="button" id="L">L</button>
-        <button type="button" id="M">M</button></BR>
-        <button type="button" id="W">W</button>
-        <button type="button" id="X">X</button>
-        <button type="button" id="C">C</button>
-        <button type="button" id="V">V</button>
-        <button type="button" id="B">B</button>
-        <button type="button" id="N">N</button></BR>
 
-        <img id ="img" src="img/pendu0.png" alt="HangmanState" height="100" width="100" ></img>
+
+        <div class="container center ">
+            <h2 class="center blue-text text-darken-2">Le Pendu</h2>
+            <div class="container">
+<?= ($hiddenWord) ?>
+            </div><a class="waves-effect waves-light btn card-panel blue darken-2" href="index.php">Nouveau Mot</a>
+            <h6 id="requestajax"><h6/>
+                <div class="raw">
+                    </BR><button class="btn waves-effect waves-light card-panel blue darken-2"  type="button" id="A">A</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="Z">Z</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="E">E</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="R">R</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="T">T</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="Y">Y</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="U">U</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="I">I</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="O">O</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="P">P</button></BR>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="Q">Q</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="S">S</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="D">D</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="F">F</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="G">G</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="H">H</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="J">J</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="K">K</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="L">L</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="M">M</button></BR>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="W">W</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="X">X</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="C">C</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="V">V</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="B">B</button>
+                    <button class="btn waves-effect waves-light card-panel blue darken-2" type="button" id="N">N</button></BR>
+                </div>
+                <img id ="img" src="img/pendu0.png" alt="HangmanState" height="250" width="250" ></img>
+        </div>
         <script>
-            j = 0;
+            loseTcheck = 0;
             winTcheck = 0;
             $("button").click(function () {
                 var lettre = $(this).attr('id');
                 var x = false;
 
                 $('#' + lettre + '').attr('disabled', 'true');
-                j++;
+
                 $.post(
                         "test.php",
                         {
@@ -108,25 +134,24 @@ echo $hiddenWord;
                             i = 0;
                             b = 0;
                             for (let test in splitMot) {
-                                if (splitMot[test]==="-"){
+                                if (splitMot[test] === "-" || splitMot[test] === "'") {
                                     lgtMot--;
                                 }
 
-                                if (splitMot[test] == lettre) {
+                                if (splitMot[test] === lettre) {
 
                                     document.getElementById(i).innerHTML = splitMot[test];
-
                                     b++;
-
                                     x = true;
 
                                 }
                                 i++;
                             }
                             if (x === false) {
-                                $('#img').attr('src', 'img/pendu' + j + '.png');
-                                if (j == 8) {
-                                    $('#requestajax').html('Perdu');
+                                loseTcheck++;
+                                $('#img').attr('src', 'img/pendu' + loseTcheck + '.png');
+                                if (loseTcheck === 8) {
+                                    $('#requestajax').html("Perdu, vous n'avez pas trouvé : " + mot);
                                     $('button').attr('disabled', 'true');
                                 }
                             } else {
@@ -136,7 +161,7 @@ echo $hiddenWord;
                                 $('#requestajax').html('Gagné');
                                 $('button').attr('disabled', 'true');
                             }
-                            console.log(winTcheck);
+
 
                         });
             });
